c95e63790f63c4539b56fc86eefcfb0c203e7789
Paul Sproxton <paul.sproxton@corel.com>
2018-08-28 19:13:52 -0400

    Add the Stats Manager project

---
+++ Stats Manager/Stats Manager/Stats Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stats_Manager
+{
+    public partial class StatsManagerForm : Form
+    {
+        public StatsManagerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
